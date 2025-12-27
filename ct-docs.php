dabe41aa9cffeee7d4930b8b447c4a305d2b6d9d
Chris Robinson <chris@contempographicdesign.com>
2025-12-27 09:11:32 -0800

    Fixed single doc mobile drawer shadow issue, v145

--- ct-docs.php
+++ ct-docs.php
@@ -3,7 +3,7 @@
  * Plugin Name: Contempo Docs
  * Plugin URI: https://contempothemes.com/
  * Description: A streamlined documentation plugin with live search, auto-generated TOC, and Elementor widgets.
- * Version: 1.4.4
+ * Version: 1.4.6
  * Author: Contempo Themes
  * Author URI: https://contempothemes.com/
  * Text Domain: ct-docs
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin Constants
  */
-define( 'CT_DOCS_VERSION', '1.4.4' );
+define( 'CT_DOCS_VERSION', '1.4.6' );
 define( 'CT_DOCS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CT_DOCS_URL', plugin_dir_url( __FILE__ ) );
 define( 'CT_DOCS_BASENAME', plugin_basename( __FILE__ ) );
